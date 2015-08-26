code-iai/iai_kinect2

commit ff3758f29719428c645cc4f8a7934d75abfbb7c0

Fixed possible deallocation of frame data before processing finised.

kinect2_bridge/src/kinect2_bridge.cpp
@@ -852,6 +852,7 @@ private:
     std::vector<cv::Mat> images(COUNT);
     std::vector<Status> status = this->status;
     size_t frame;
+    std::shared_ptr<libfreenect2::Frame> irFrame, depthFrame;
 
     if(!receiveFrames(listenerIrDepth, frames))
     {
@@ -864,6 +865,8 @@ private:
 
     irFrame = std::shared_ptr<libfreenect2::Frame>(frames[libfreenect2::Frame::Ir]);
     depthFrame = std::shared_ptr<libfreenect2::Frame>(frames[libfreenect2::Frame::Depth]);
+    this->irFrame = irFrame;
+    this->depthFrame = depthFrame;
 
     ir = cv::Mat(irFrame->height, irFrame->width, CV_32FC1, irFrame->data);
     depth = cv::Mat(depthFrame->height, depthFrame->width, CV_32FC1, depthFrame->data);
@@ -889,6 +892,7 @@ private:
     std::vector<cv::Mat> images(COUNT);
     std::vector<Status> status = this->status;
     size_t frame;
+    std::shared_ptr<libfreenect2::Frame> colorFrame;
 
     if(!receiveFrames(listenerColor, frames))
     {
@@ -900,6 +904,7 @@ private:
     header = createHeader(lastColor, lastDepth);
 
     colorFrame = std::shared_ptr<libfreenect2::Frame>(frames[libfreenect2::Frame::Color]);
+    this->colorFrame = colorFrame;
 
     color = cv::Mat(colorFrame->height, colorFrame->width, CV_8UC4, colorFrame->data);
 
@@ -1368,7 +1373,7 @@ void help(const std::string &path)
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "kinect2_bridge");
+  ros::init(argc, argv, "kinect2_bridge", ros::init_options::AnonymousName);
 
 
   for(int argI = 1; argI < argc; ++argI)

commit f7b0cee0cf5b2b6a94193f348c7de30712d8c40d

fix for crash introduced in commit ff3758f29719428c645cc4f8a7934d75abfbb7c0

kinect2_bridge/src/kinect2_bridge.cpp
@@ -63,7 +63,7 @@ private:
   cv::Mat map1Color, map2Color, map1Ir, map2Ir, map1LowRes, map2LowRes;
 
   std::vector<std::thread> threads;
-  std::mutex lockIrDepth, lockColor;
+  std::mutex lockIrDepth, lockColor, lockColorFrame;
   std::mutex lockSync, lockPub, lockTime, lockStatus;
   std::mutex lockRegLowRes, lockRegHighRes;
 
@@ -77,7 +77,7 @@ private:
   libfreenect2::Registration *registration;
   libfreenect2::Freenect2Device::ColorCameraParams colorParams;
   libfreenect2::Freenect2Device::IrCameraParams irParams;
-  std::shared_ptr<libfreenect2::Frame> irFrame, depthFrame, colorFrame;
+  libfreenect2::Frame colorFrame;
 
   ros::NodeHandle nh, priv_nh;
 
@@ -129,13 +129,14 @@ private:
 
 public:
   Kinect2Bridge(const ros::NodeHandle &nh = ros::NodeHandle(), const ros::NodeHandle &priv_nh = ros::NodeHandle("~"))
-    : sizeColor(1920, 1080), sizeIr(512, 424), sizeLowRes(sizeColor.width / 2, sizeColor.height / 2), nh(nh), priv_nh(priv_nh),
+    : sizeColor(1920, 1080), sizeIr(512, 424), sizeLowRes(sizeColor.width / 2, sizeColor.height / 2), colorFrame(1920, 1080, 4), nh(nh), priv_nh(priv_nh),
       frameColor(0), frameIrDepth(0), pubFrameColor(0), pubFrameIrDepth(0), lastColor(0, 0), lastDepth(0, 0), nextColor(false),
       nextIrDepth(false), depthShift(0), running(false), deviceActive(false), clientConnected(false)
   {
     color = cv::Mat::zeros(sizeColor, CV_8UC3);
     ir = cv::Mat::zeros(sizeIr, CV_32F);
     depth = cv::Mat::zeros(sizeIr, CV_32F);
+    memset(colorFrame.data, 0, colorFrame.width * colorFrame.height * colorFrame.bytes_per_pixel);
 
     status.resize(COUNT, UNSUBCRIBED);
   }
@@ -852,7 +853,6 @@ private:
     std::vector<cv::Mat> images(COUNT);
     std::vector<Status> status = this->status;
     size_t frame;
-    std::shared_ptr<libfreenect2::Frame> irFrame, depthFrame;
 
     if(!receiveFrames(listenerIrDepth, frames))
     {
@@ -863,10 +863,8 @@ private:
 
     header = createHeader(lastDepth, lastColor);
 
-    irFrame = std::shared_ptr<libfreenect2::Frame>(frames[libfreenect2::Frame::Ir]);
-    depthFrame = std::shared_ptr<libfreenect2::Frame>(frames[libfreenect2::Frame::Depth]);
-    this->irFrame = irFrame;
-    this->depthFrame = depthFrame;
+    libfreenect2::Frame *irFrame = frames[libfreenect2::Frame::Ir];
+    libfreenect2::Frame *depthFrame = frames[libfreenect2::Frame::Depth];
 
     ir = cv::Mat(irFrame->height, irFrame->width, CV_32FC1, irFrame->data);
     depth = cv::Mat(depthFrame->height, depthFrame->width, CV_32FC1, depthFrame->data);
@@ -874,9 +872,11 @@ private:
     frame = frameIrDepth++;
     lockIrDepth.unlock();
 
-    processIrDepth(ir, depth, images, status);
+    processIrDepth(ir, depth, images, status, depthFrame);
 
     publishImages(images, header, status, frame, pubFrameIrDepth, IR_SD, COLOR_HD);
+
+    listenerIrDepth->release(frames);
 
     double elapsed = ros::Time::now().toSec() - now;
     lockTime.lock();
@@ -892,7 +892,6 @@ private:
     std::vector<cv::Mat> images(COUNT);
     std::vector<Status> status = this->status;
     size_t frame;
-    std::shared_ptr<libfreenect2::Frame> colorFrame;
 
     if(!receiveFrames(listenerColor, frames))
     {
@@ -903,17 +902,18 @@ private:
 
     header = createHeader(lastColor, lastDepth);
 
-    colorFrame = std::shared_ptr<libfreenect2::Frame>(frames[libfreenect2::Frame::Color]);
-    this->colorFrame = colorFrame;
+    libfreenect2::Frame *colorFrame = frames[libfreenect2::Frame::Color];
 
     color = cv::Mat(colorFrame->height, colorFrame->width, CV_8UC4, colorFrame->data);
 
     frame = frameColor++;
     lockColor.unlock();
 
-    processColor(color, images, status);
+    processColor(color, images, status, colorFrame);
 
     publishImages(images, header, status, frame, pubFrameColor, COLOR_HD, COUNT);
+
+    listenerColor->release(frames);
 
     double elapsed = ros::Time::now().toSec() - now;
     lockTime.lock();
@@ -962,30 +962,21 @@ private:
     std_msgs::Header header;
     header.seq = 0;
     header.stamp = timestamp;
-    header.frame_id = K2_TF_RGB_OPT_FRAME;
     return header;
   }
 
-  void processIrDepth(const cv::Mat &ir, const cv::Mat &depth, std::vector<cv::Mat> &images, const std::vector<Status> &status)
+  void processIrDepth(const cv::Mat &ir, const cv::Mat &depth, std::vector<cv::Mat> &images, const std::vector<Status> &status, libfreenect2::Frame *depthFrame)
   {
     // COLOR registered to depth
     if(status[COLOR_SD_RECT])
     {
-      if(!colorFrame)
-      {
-        images[COLOR_SD_RECT] = cv::Mat::zeros(sizeIr, CV_8UC3);
-      }
-      else
-      {
-        std::shared_ptr<libfreenect2::Frame> tmpColor, tmpDepth;
-        cv::Mat tmp;
-        libfreenect2::Frame undistorted(sizeIr.width, sizeIr.height, 4), registered(sizeIr.width, sizeIr.height, 4);
-        tmpColor = colorFrame;
-        tmpDepth = depthFrame;
-        registration->apply(tmpColor.get(), tmpDepth.get(), &undistorted, &registered);
-        cv::flip(cv::Mat(sizeIr, CV_8UC4, registered.data), tmp, 1);
-        cv::cvtColor(tmp, images[COLOR_SD_RECT], CV_BGRA2BGR);
-      }
+      cv::Mat tmp;
+      libfreenect2::Frame undistorted(sizeIr.width, sizeIr.height, 4), registered(sizeIr.width, sizeIr.height, 4);
+      lockColorFrame.lock();
+      registration->apply(&colorFrame, depthFrame, &undistorted, &registered);
+      lockColorFrame.unlock();
+      cv::flip(cv::Mat(sizeIr, CV_8UC4, registered.data), tmp, 1);
+      cv::cvtColor(tmp, images[COLOR_SD_RECT], CV_BGRA2BGR);
     }
 
     // IR
@@ -1029,8 +1020,18 @@ private:
     }
   }
 
-  void processColor(const cv::Mat &color, std::vector<cv::Mat> &images, const std::vector<Status> &status)
+  void processColor(const cv::Mat &color, std::vector<cv::Mat> &images, const std::vector<Status> &status, libfreenect2::Frame *colorFrame)
   {
+    if(status[COLOR_SD_RECT])
+    {
+      this->colorFrame.timestamp = colorFrame->timestamp;
+      this->colorFrame.sequence = colorFrame->sequence;
+      size_t size = colorFrame->height * colorFrame->width * colorFrame->bytes_per_pixel;
+      lockColorFrame.lock();
+      memcpy(this->colorFrame.data, colorFrame->data, size);
+      lockColorFrame.unlock();
+    }
+
     // COLOR
     if(status[COLOR_HD] || status[COLOR_HD_RECT] || status[COLOR_QHD] || status[COLOR_QHD_RECT] ||
        status[MONO_HD] || status[MONO_HD_RECT] || status[MONO_QHD] || status[MONO_QHD_RECT])